40e52e8bcc51fec4b9cf9a98f414aedc4b6a01ff
JoseMunozO <josecarlosm98@gmail.com>
2026-04-29 10:52:38 +0200

    first commit

---
+++ src/test/java/com/preschool/backendpreschool/BackendPreschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preschool.backendpreschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPreschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
